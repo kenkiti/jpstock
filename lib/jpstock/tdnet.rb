c5be42e0dee2a2db3f7c31092a24d8f32dc6455c
utahta <labs.ninxit@gmail.com>
2013-07-21 23:03:49 +0900

    xbrlリンク追加。tdnet_each追加。

--- lib/jpstock/tdnet.rb
+++ lib/jpstock/tdnet.rb
@@ -5,8 +5,8 @@ module JpStock
   end
   
   class TdnetData
-    attr_accessor :datetime, :code, :company_name, :title, :url, :gyouhai, :exchange
-    def initialize(datetime, code, company_name, title, url, gyouhai, exchange)
+    attr_accessor :datetime, :code, :company_name, :title, :url, :gyouhai, :exchange, :xbrl
+    def initialize(datetime, code, company_name, title, url, gyouhai, exchange, xbrl)
       @datetime = datetime # 日時
       @code = code # 証券コード
       @company_name = company_name # 会社名
@@ -14,13 +14,14 @@ module JpStock
       @url = url # PDFリンク
       @gyouhai = gyouhai # 業績・配当
       @exchange = exchange # 取引所
+      @xbrl = xbrl # XBRLリンク
     end
   end
   
-  # 適時開示情報を取得
+  # 適時開示情報を順番に取得
   # :code 証券コード
   # :date 日付
-  def tdnet(options={})
+  def tdnet_each(options={})
     if options.nil? or !options.is_a?(Hash)
       raise TdnetException, "オプションがnil、もしくはハッシュじゃないです"
     end
@@ -65,47 +66,57 @@ module JpStock
     
     results = []
     100.times do |i|
-        num = "%03d" % (i+1)
-        pageurl = "#{root_url}/I_list_#{num}_#{year}#{month}#{day}.html"
+      num = "%03d" % (i+1)
+      pageurl = "#{root_url}/I_list_#{num}_#{year}#{month}#{day}.html"
 
-        print "Fetching #{pageurl}...\n"
-        begin
-            html = open(pageurl)
-        rescue
-            break
-        end
-        doc = Nokogiri::HTML(html)
+      print "Fetching #{pageurl}...\n"
+      begin
+          html = open(pageurl)
+      rescue
+          break
+      end
+      doc = Nokogiri::HTML(html)
+      
+      # 情報取得
+      tables = doc.xpath('//table[@cellspacing="0"]')
+      trs = tables[0].xpath('.//tr')
+      trs.shift
+  
+      trs.each do |tr|
+        tds = tr.xpath('.//td')
+        row = tds.map{|td| td.content.strip}
         
-        # 情報取得
-        tables = doc.xpath('//table[@cellspacing="0"]')
-        trs = tables[0].xpath('.//tr')
-        trs.shift
-    
-        trs.each do |tr|
-            tds = tr.xpath('.//td')
-            row = tds.map{|td| td.content.strip}
-            
-            datetime = DateTime.strptime("#{year}/#{month}/#{day} #{row[0]} JST", '%Y/%m/%d %H:%M %Z') # 日付時刻
-            code = row[1][0..3] # コード
-            name = row[2] # 会社名
-            title = row[3] # 表題
-            url = tds[3].xpath('.//a') # 開示リンク先
-            url = url.empty? ? "#" :  "#{root_url}/"+url[0][:href]
-            gyouhai = !row[4].empty? # 業績・配当
-            exchange = row[6] # 取引所
-            
-            if all
-              results.push(TdnetData.new(datetime, code, name, title, url, gyouhai, exchange))
-            else
-              if codes.include?(code)
-                results.push(TdnetData.new(datetime, code, name, title, url, gyouhai, exchange))
-              end
-            end 
-        end
-        sleep(0.5)
+        datetime = DateTime.strptime("#{year}/#{month}/#{day} #{row[0]} JST", '%Y/%m/%d %H:%M %Z') # 日付時刻
+        code = row[1][0..3] # コード
+        name = row[2] # 会社名
+        title = row[3] # 表題
+        url = tds[3].xpath('.//a') # 開示リンク先
+        url = url.empty? ? "" :  "#{root_url}/"+url[0][:href]
+        gyouhai = !row[4].empty? # 業績・配当
+        xbrl = tds[5].xpath('.//a') # XBRLリンク先
+        xbrl = xbrl.empty? ? "" : "#{root_url}/"+xbrl[0][:href]
+        exchange = row[6] # 取引所
+        
+        if all
+          yield TdnetData.new(datetime, code, name, title, url, gyouhai, exchange, xbrl)
+        else
+          if codes.include?(code)
+            yield TdnetData.new(datetime, code, name, title, url, gyouhai, exchange, xbrl)
+          end
+        end 
+      end
     end
-    return results
   end
-    
-  module_function :tdnet
+  
+  # 適時開示情報をまとめて取得
+  def tdnet(options={})
+    results = []
+    tdnet_each(options) do |tdnet|
+      results.push(tdnet)
+      sleep(0.3)
+    end
+    results
+  end
+  
+  module_function :tdnet_each, :tdnet
 end
